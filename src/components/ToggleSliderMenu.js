d9cbfb474c042d1f6b737b6fce26410c67520962
LeeSangMin1029 <sangmins39@gmail.com>
2020-10-06 07:10:47 +0900

    Add click-hide menu

---
+++ src/components/ToggleSliderMenu.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../style/ToggleSliderMenu.scss';
+
+const ToggleSliderMenu = () => {
+  return (
+    <div className='ToggleSliderMenu'>
+      <div className='ta-c'>
+        <span>Information</span>
+      </div>
+      <div className='Contact'>
+        <a href='https://github.com/LeeSangMin1029/Calendar'>github</a>
+      </div>
+    </div>
+  );
+};
+
+export default ToggleSliderMenu;
