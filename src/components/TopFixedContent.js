6f5a96789e656b31f70c75735e745265d96d5e61
LeeSangMin1029 <sangmins39@gmail.com>
2020-10-06 07:04:11 +0900

    헤더 추가

---
+++ src/components/TopFixedContent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../style/TopFixedContent.scss';
+
+const TopFixedContent = () => {
+  return (
+    <header className='TopFixedContent'>
+      <div className='Info'>
+        <div className='HeaderTitle'>
+          <span>캘린더</span>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default TopFixedContent;
