e50c40d870380df508652d5cbc019d9d7302ecbc
LeeSangMin1029 <sangmins39@gmail.com>
2020-10-06 06:53:04 +0900

    전체 앱의 스타일 적용

--- src/components/CalendarTemplate.js
+++ src/components/CalendarTemplate.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import '../style/CalendarTemplate.scss';
 
 const CalendarTemplate = ({ children }) => {
-  return <div>{children}</div>;
+  return <div className='CalendarTemplate'>{children}</div>;
 };
 
 export default CalendarTemplate;
