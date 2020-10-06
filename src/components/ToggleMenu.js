abab0c7959a97eecb7a469b5da2acad14712e191
LeeSangMin1029 <sangmins39@gmail.com>
2020-10-06 13:31:56 +0900

    className으로 변경 및 조건에 따라 그려지도록 수정

--- src/components/ToggleMenu.js
+++ src/components/ToggleMenu.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import '../style/ToggleMenu.scss';
 
-const ToggleMenu = () => {
+const ToggleMenu = ({ isOpenMenu }) => {
   return (
-    <div class='ToggleMenu'>
-      <span class='MenuItem'>
-        <div class='txt-center'>일</div>
-        <span>D</span>
-      </span>
-      <span class='MenuItem'>
-        <div class='txt-center'>주</div>
-        <span>W</span>
-      </span>
-      <span class='MenuItem'>
-        <div class='txt-center'>월</div>
-        <span>M</span>
-      </span>
-      <span class='MenuItem'>
-        <div class='txt-center'>연도</div>
-        <span>Y</span>
-      </span>
-    </div>
+    <>
+      {isOpenMenu && (
+        <div className='ToggleMenu'>
+          <span className='MenuItem'>
+            <div className='txt-center'>일</div>
+            <span>D</span>
+          </span>
+          <span className='MenuItem'>
+            <div className='txt-center'>주</div>
+            <span>W</span>
+          </span>
+          <span className='MenuItem'>
+            <div className='txt-center'>월</div>
+            <span>M</span>
+          </span>
+          <span className='MenuItem'>
+            <div className='txt-center'>연도</div>
+            <span>Y</span>
+          </span>
+        </div>
+      )}
+    </>
   );
 };
 
