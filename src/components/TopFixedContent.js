887056bdea6e41733df5828e03f1ff122090fa42
LeeSangMin1029 <sangmins39@gmail.com>
2020-10-06 13:34:16 +0900

    useState로 메뉴 컴포넌트의 렌더링 여부값을 저장
    클릭할 때마다 메뉴가 나타났다 사라졌다 반복한다

--- src/components/TopFixedContent.js
+++ src/components/TopFixedContent.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ToggleMenu from './ToggleMenu';
 import '../style/TopFixedContent.scss';
 import expandArrow from '../assets/image/expand-arrow.png';
 
 const TopFixedContent = () => {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+
   return (
     <header className='TopFixedContent'>
       <div className='Info'>
         <div className='HeaderTitle'>
           <span>캘린더</span>
         </div>
-        <div className='ClickMenu'>
+        <div
+          className='ClickMenu'
+          onClick={() => {
+            setIsOpenMenu(!isOpenMenu);
+          }}
+        >
           <span className='txt'>연도</span>
           <img src={expandArrow} width='20' height='20' alt='expandArrow' />
         </div>
+        <ToggleMenu isOpenMenu={isOpenMenu} />
       </div>
     </header>
   );
