4bffc46fb25a24365c62f4eb479dfaa7fa638d8d
LeeSangMin1029 <sangmins39@gmail.com>
2020-10-10 20:07:50 +0900

    [#13] ToggleMenu 기능 업데이트
    - 이전에 요청했던 주소와 현재 요청한 주소 같을 때 중복 방지
    - 메뉴를 열어놓은 상태에서 다른 컴포넌트를 클릭했을 때 닫히는 기능 추가
    - 메뉴를 열어놓은 상태에서 브라우저 기록 상태가 변경될 때 닫히는 기능
    추가
    - 메뉴의 버튼들을 클릭했을 때 닫히는 기능 추가
    Resolve #13

--- src/components/ToggleMenu.js
+++ src/components/ToggleMenu.js
@@ -1,22 +1,64 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
+import React, { useRef, useState, useEffect, useCallback } from 'react';
+import { withRouter, useHistory } from 'react-router-dom';
+import useOutSideClick from './useOutsideClick';
 import MenuItem from './MenuItem';
+
+import expandArrow from '../assets/image/expand-arrow.png';
 import '../style/ToggleMenu.scss';
 
-const listItemInfo = [
+const menuItemList = [
   { kor: '일', eng: 'D', id: 'itemD', path: '/day' },
   { kor: '주', eng: 'W', id: 'itemW', path: '/week' },
   { kor: '월', eng: 'M', id: 'itemM', path: '/month' },
   { kor: '연도', eng: 'Y', id: 'itemY', path: '/year' },
 ];
 
-const ToggleMenu = ({ isOpenMenu }) => {
+const createMenuName = (path = '/') => {
+  const matchedItem = menuItemList.find((item) => item.path === path || item.path === '/year');
+  return matchedItem.kor;
+};
+
+const ToggleMenu = () => {
+  const history = useHistory();
+  const [name, setName] = useState(createMenuName(window.location.pathname));
+  const [isOpen, setIsOpen] = useState(false);
+  const ref = useRef();
+
+  const onCloseMenu = useCallback(() => {
+    if (isOpen) setIsOpen(false);
+  }, [isOpen]);
+
+  const onToggleMenu = () => {
+    setIsOpen((prevIsOpen) => !prevIsOpen);
+  };
+
+  const onUpdateMenu = (name, target) => {
+    setName(name);
+    onCloseMenu();
+    history.push(target);
+  };
+
+  useOutSideClick(ref, () => {
+    onCloseMenu();
+  });
+
+  useEffect(() => {
+    return history.listen((location) => {
+      setName(createMenuName(location.pathname));
+      onCloseMenu();
+    });
+  }, [history, onCloseMenu]);
+
   return (
     <>
-      {isOpenMenu && (
+      <div className='click-menu' onClick={onToggleMenu} ref={ref}>
+        <span className='txt'>{name}</span>
+        <img src={expandArrow} width='20' height='20' alt='expandArrow' />
+      </div>
+      {isOpen && (
         <div className='ToggleMenu'>
-          {listItemInfo.map((item) => (
-            <MenuItem item={item} key={item.id} />
+          {menuItemList.map((item) => (
+            <MenuItem item={item} key={item.id} onUpdateMenu={onUpdateMenu} />
           ))}
         </div>
       )}
