03a53dc7955bd43d672cb011bc982afc06291cf1
LeeSangMin1029 <sangmins39@gmail.com>
2020-10-10 20:07:12 +0900

    ToggleMenu 관련 기능 분리
    메뉴 토글 시 해당 컴포넌트만 rerendering

--- src/components/TopFixedContent.js
+++ src/components/TopFixedContent.js
@@ -1,27 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ToggleMenu from './ToggleMenu';
+
 import '../style/TopFixedContent.scss';
-import expandArrow from '../assets/image/expand-arrow.png';
 
 const TopFixedContent = () => {
-  const [isOpenMenu, setIsOpenMenu] = useState(false);
-
   return (
     <header className='TopFixedContent'>
       <div className='info'>
         <div className='header-title'>
           <span>캘린더</span>
         </div>
-        <div
-          className='click-menu'
-          onClick={() => {
-            setIsOpenMenu(!isOpenMenu);
-          }}
-        >
-          <span className='txt'>연도</span>
-          <img src={expandArrow} width='20' height='20' alt='expandArrow' />
-        </div>
-        <ToggleMenu isOpenMenu={isOpenMenu} />
+        <ToggleMenu />
       </div>
     </header>
   );
