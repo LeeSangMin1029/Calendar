75d81d4f6b63067888dfa97097c646bb6ff3a9cf
LeeSangMin1029 <sangmins39@gmail.com>
2020-10-16 21:25:42 +0900

    Template으로 이름 변경

--- src/components/CalendarTemplate.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../style/CalendarTemplate.scss';
-
-const CalendarTemplate = ({ children }) => {
-  return <div className='CalendarTemplate'>{children}</div>;
-};
-
-export default CalendarTemplate;
